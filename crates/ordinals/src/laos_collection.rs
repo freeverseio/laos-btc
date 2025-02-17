d7a43ae65cdd7c63ee3946328b92d865add99ad0
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-02-17 13:43:25 +0100

    Laos Collection decipher

--- crates/ordinals/src/laos_collection.rs
+++ crates/ordinals/src/laos_collection.rs
@@ -38,71 +38,14 @@ impl LaosCollection {
 	pub fn decipher(transaction: &Transaction) -> Option<LaosCollection> {
 		let payload = LaosCollection::payload(transaction)?;
 
-		let Message { collection_address, rebaseable } =
+		let message =
 			Message::from_payload(payload);
-
-		let etching = Flag::Etching.take(&mut flags).then(|| Etching {
-			divisibility: Tag::Divisibility.take(&mut fields, |[divisibility]| {
-				let divisibility = u8::try_from(divisibility).ok()?;
-				(divisibility <= Etching::MAX_DIVISIBILITY).then_some(divisibility)
-			}),
-			premine: Tag::Premine.take(&mut fields, |[premine]| Some(premine)),
-			rune: Tag::Rune.take(&mut fields, |[rune]| Some(Rune(rune))),
-			spacers: Tag::Spacers.take(&mut fields, |[spacers]| {
-				let spacers = u32::try_from(spacers).ok()?;
-				(spacers <= Etching::MAX_SPACERS).then_some(spacers)
-			}),
-			symbol: Tag::Symbol
-				.take(&mut fields, |[symbol]| char::from_u32(u32::try_from(symbol).ok()?)),
-			terms: Flag::Terms.take(&mut flags).then(|| Terms {
-				cap: Tag::Cap.take(&mut fields, |[cap]| Some(cap)),
-				height: (
-					Tag::HeightStart
-						.take(&mut fields, |[start_height]| u64::try_from(start_height).ok()),
-					Tag::HeightEnd
-						.take(&mut fields, |[start_height]| u64::try_from(start_height).ok()),
-				),
-				amount: Tag::Amount.take(&mut fields, |[amount]| Some(amount)),
-				offset: (
-					Tag::OffsetStart
-						.take(&mut fields, |[start_offset]| u64::try_from(start_offset).ok()),
-					Tag::OffsetEnd.take(&mut fields, |[end_offset]| u64::try_from(end_offset).ok()),
-				),
-			}),
-			turbo: Flag::Turbo.take(&mut flags),
-		});
 
 		let mint = Tag::Mint.take(&mut fields, |[block, tx]| {
 			RuneId::new(block.try_into().ok()?, tx.try_into().ok()?)
 		});
 
-		let pointer = Tag::Pointer.take(&mut fields, |[pointer]| {
-			let pointer = u32::try_from(pointer).ok()?;
-			(u64::from(pointer) < u64::try_from(transaction.output.len()).unwrap())
-				.then_some(pointer)
-		});
-
-		if etching.map(|etching| etching.supply().is_none()).unwrap_or_default() {
-			flaw.get_or_insert(Flaw::SupplyOverflow);
-		}
-
-		if flags != 0 {
-			flaw.get_or_insert(Flaw::UnrecognizedFlag);
-		}
-
-		if fields.keys().any(|tag| tag % 2 == 0) {
-			flaw.get_or_insert(Flaw::UnrecognizedEvenTag);
-		}
-
-		if let Some(flaw) = flaw {
-			return Some(Artifact::Cenotaph(Cenotaph {
-				flaw: Some(flaw),
-				mint,
-				etching: etching.and_then(|etching| etching.rune),
-			}));
-		}
-
-		Some(Artifact::LaosCollection(Self { edicts, etching, mint, pointer }))
+		Some(Self { message, mint })
 	}
 
 	pub fn encipher(&self) -> ScriptBuf {
