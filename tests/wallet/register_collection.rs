1fae6a488fce33da9e70723eac9ae31350ff7617
magecnion <magecnion@gmail.com>
2025-02-19 16:51:36 +0100

    postage tests

--- tests/wallet/register_collection.rs
+++ tests/wallet/register_collection.rs
@@ -1,5 +1,5 @@
 use super::*;
-use ord::subcommand::wallet::register_collection;
+use ord::{bridgeless_minting::bitcoin_service, subcommand::wallet::register_collection};
 use sp_core::H160;
 
 #[test]
@@ -58,4 +58,79 @@ fn rebaseable_is_false_by_default() {
 	let tx = core.tx_by_id(output.tx_id);
 	let register_collection = RegisterCollection::decipher(&tx).unwrap();
 	assert!(!register_collection.rebaseable);
+}
+
+#[test]
+fn calculate_postage_amount_by_default() {
+	let core = mockcore::builder().network(Network::Regtest).build();
+
+	let ord = TestServer::spawn_with_server_args(&core, &["--regtest"], &[]);
+
+	core.mine_blocks(1);
+
+	create_wallet(&core, &ord);
+
+	let address = CommandBuilder::new("--regtest wallet receive")
+		.core(&core)
+		.ord(&ord)
+		.run_and_deserialize_output::<ord::subcommand::wallet::receive::Output>()
+		.addresses
+		.into_iter()
+		.next()
+		.unwrap();
+	let address = address.require_network(core.state().network).unwrap();
+
+	let postage = bitcoin_service::calculate_postage(None, address.clone()).unwrap();
+	assert_eq!(postage.amount, Amount::from_sat(10_000));
+	assert_eq!(postage.destination, address);
+}
+
+#[test]
+fn calculate_postage_when_amount_is_provided() {
+	let core = mockcore::builder().network(Network::Regtest).build();
+
+	let ord = TestServer::spawn_with_server_args(&core, &["--regtest"], &[]);
+
+	core.mine_blocks(1);
+
+	create_wallet(&core, &ord);
+
+	let address = CommandBuilder::new("--regtest wallet receive")
+		.core(&core)
+		.ord(&ord)
+		.run_and_deserialize_output::<ord::subcommand::wallet::receive::Output>()
+		.addresses
+		.into_iter()
+		.next()
+		.unwrap();
+	let address = address.require_network(core.state().network).unwrap();
+
+	let amount = Amount::from_sat(20_000);
+	let postage = bitcoin_service::calculate_postage(Some(amount), address.clone()).unwrap();
+	assert_eq!(postage.amount, amount);
+	assert_eq!(postage.destination, address);
+}
+
+#[test]
+fn calculate_postage_when_low_amount_is_provided() {
+	let core = mockcore::builder().network(Network::Regtest).build();
+
+	let ord = TestServer::spawn_with_server_args(&core, &["--regtest"], &[]);
+
+	core.mine_blocks(1);
+
+	create_wallet(&core, &ord);
+
+	let address = CommandBuilder::new("--regtest wallet receive")
+		.core(&core)
+		.ord(&ord)
+		.run_and_deserialize_output::<ord::subcommand::wallet::receive::Output>()
+		.addresses
+		.into_iter()
+		.next()
+		.unwrap();
+	let address = address.require_network(core.state().network).unwrap();
+
+	let amount = Amount::from_sat(1);
+	assert!(bitcoin_service::calculate_postage(Some(amount), address.clone()).is_err(), "postage below dust limit of 330sat");
 }
