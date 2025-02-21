f0c78f5e5b0cc123536696cde0c827c15ffe7e58
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-02-21 13:23:32 +0100

    Add register collection + indexer integration test

--- tests/wallet/brc721/register_collection.rs
+++ tests/wallet/brc721/register_collection.rs
@@ -1,5 +1,5 @@
 use super::*;
-use ord::subcommand::wallet::brc721::register_collection;
+use ord::{subcommand::wallet::brc721::register_collection, templates::Brc721CollectionsHtml};
 use sp_core::H160;
 
 #[test]
@@ -58,4 +58,41 @@ fn rebaseable_is_false_by_default() {
 	let tx = core.tx_by_id(output.tx_id);
 	let register_collection = RegisterCollection::decipher(&tx).unwrap();
 	assert!(!register_collection.rebaseable);
+}
+
+#[test]
+fn register_collection_command_indexer_integration() {
+	let core = mockcore::builder().network(Network::Regtest).build();
+
+	let ord = TestServer::spawn_with_server_args(&core, &["--regtest"], &[]);
+
+	core.mine_blocks(1);
+
+	create_wallet(&core, &ord);
+
+	let alice = H160::from_slice(&[0; 20]);
+
+	CommandBuilder::new(format!(
+		"--regtest wallet brc721 register-collection --fee-rate 1 --address {:x} --rebaseable",
+		alice
+	))
+	.core(&core)
+	.ord(&ord)
+	.expected_exit_code(0)
+	.run_and_deserialize_output::<register_collection::Output>();
+
+	core.mine_blocks(1);
+	ord.assert_html(
+		"/brc721/collections",
+		Chain::Regtest,
+		Brc721CollectionsHtml {
+			entries: vec![(
+				RuneId { block: 2, tx: 1 },
+				"0x0000000000000000000000000000000000000000".to_owned(),
+			)],
+			more: false,
+			prev: None,
+			next: None,
+		},
+	);
 }
