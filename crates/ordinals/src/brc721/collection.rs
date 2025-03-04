b88ba80627da0a68e8c3dbc03aa04743fc206831
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-03-04 10:57:24 +0100

    using named struct

--- crates/ordinals/src/brc721/collection.rs
+++ crates/ordinals/src/brc721/collection.rs
@@ -22,3 +22,47 @@ impl fmt::Display for Brc721Collection {
 		write!(f, "{} - {:?} - {}", self.collection_id, self.address, self.rebaseable)
 	}
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hex_literal::hex;
+
+    #[test]
+    fn test_brc721_collection_creation() {
+        let collection_id = Brc721CollectionId::new(); // Assuming you have a method to create a new ID
+        let address = H160::from(hex!("0000000000000000000000000000000000000001"));
+        let rebaseable = true;
+
+        let collection = Brc721Collection::new(collection_id.clone(), address, rebaseable);
+
+        assert_eq!(collection.collection_id, collection_id);
+        assert_eq!(collection.address, address);
+        assert_eq!(collection.rebaseable, rebaseable);
+    }
+
+    #[test]
+    fn test_brc721_collection_display() {
+        let collection_id = Brc721CollectionId::new(); // Create a collection ID
+        let address = H160::from(hex!("0000000000000000000000000000000000000001"));
+        let rebaseable = false;
+
+        let collection = Brc721Collection::new(collection_id.clone(), address, rebaseable);
+
+        let expected_display = format!("{} - {:?} - {}", collection_id, address, rebaseable);
+        assert_eq!(format!("{}", collection), expected_display);
+    }
+
+    #[test]
+    fn test_brc721_collection_equality() {
+        let collection_id = Brc721CollectionId::new(); // Assume this method exists
+        let address = H160::from(hex!("0000000000000000000000000000000000000001"));
+        
+        let collection_a = Brc721Collection::new(collection_id.clone(), address, true);
+        let collection_b = Brc721Collection::new(collection_id.clone(), address, true);
+        let collection_c = Brc721Collection::new(collection_id.clone(), address, false);
+
+        assert_eq!(collection_a, collection_b);
+        assert_ne!(collection_a, collection_c);
+    }
+}
