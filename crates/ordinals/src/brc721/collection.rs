6d5cc1cff2bd3b3dbb1b45538fcf352b06e33bf3
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-03-04 10:50:18 +0100

    fone

--- crates/ordinals/src/brc721/collection.rs
+++ crates/ordinals/src/brc721/collection.rs
@@ -1,6 +1,13 @@
 use super::collection_id::Brc721CollectionId;
 use crate::{Deserialize, Serialize};
 use sp_core::H160;
+use std::fmt;
 
 #[derive(Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Brc721Collection(pub Brc721CollectionId, pub H160, pub bool);
+
+impl fmt::Display for Brc721Collection {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "{} - {:?} - {}", self.0, self.1, self.2)
+	}
+}
--- src/templates/brc721_collections.rs
+++ src/templates/brc721_collections.rs
@@ -50,7 +50,7 @@ mod tests {
 			.to_string(),
 			"<h1>Brc721 Collections</h1>
 <ul>
-  <li>0:0 - 0x0000…0000 - false</li>
+  <li>0:0 - 0x0000000000000000000000000000000000000000 - false</li>
 </ul>
 <div class=center>
     prev
@@ -82,8 +82,8 @@ mod tests {
 			.to_string(),
 			"<h1>Brc721 Collections</h1>
 <ul>
-  <li>0:0 - 0x0000…0000 - false</li>
-  <li>1:1 - 0x0000…0001 - false</li>
+  <li>0:0 - 0x0000000000000000000000000000000000000000 - false</li>
+  <li>1:1 - 0x0000000000000000000000000000000000000001 - false</li>
 </ul>
 <div class=center>
     <a class=prev href=/brc721/collections/1>prev</a>
