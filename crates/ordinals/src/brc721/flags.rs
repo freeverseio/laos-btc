065dc3af160d4ac6ffbede80bca48c110f66770a
Tomás Senovilla Polo <117524919+tsenovilla@users.noreply.github.com>
2025-03-05 16:27:51 +0100

    Update crates/ordinals/src/brc721/flags.rs
    Co-authored-by: magecnion <magecnion@gmail.com>

--- crates/ordinals/src/brc721/flags.rs
+++ crates/ordinals/src/brc721/flags.rs
@@ -3,7 +3,7 @@ use bitcoin::opcodes;
 /// The opcode used to identify register collection operations.
 pub(crate) const BRC721_INIT_CODE: opcodes::Opcode = opcodes::all::OP_PUSHNUM_15;
 
-/// Byte flag to identify which mode of brc721 is used. Just a fancy name to don't remember the
+/// Byte to identify which operation of brc721 is used. Just a fancy name to don't remember the
 /// numerical byte of each operation. Only fieldless variants are allowed, leading to a compile
 /// error otherwise.
 #[derive(Debug, Clone, PartialEq)]
