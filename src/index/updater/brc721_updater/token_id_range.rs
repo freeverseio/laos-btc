9f25c2609ab14e7e09b4422c36031d94abd662cb
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-03-26 15:19:36 +0100

    using real addresses

--- src/index/updater/brc721_updater/token_id_range.rs
+++ src/index/updater/brc721_updater/token_id_range.rs
@@ -24,20 +24,21 @@ impl TokenIdRange {
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use std::str::FromStr;
 
 	#[test]
 	fn get_first_token_id() {
 		// crate the Range
 		let first_slot = Slot::try_from(1).unwrap();
 		let last_slot = Slot::try_from(9).unwrap();
-		let registrant = H160::default();
+		let registrant = H160::from_str("0xD4a24FE19b5e0ED77137012B95b4433293E2Ff8E").unwrap();
 		let range = TokenIdRange::new(first_slot, last_slot, registrant);
 
 		let first_token: U256 = range.first_token().into();
 
 		let hex_string = hex::encode(first_token.to_big_endian());
-		assert_eq!(hex_string, "0000000000000000000000010000000000000000000000000000000000000000");
-		assert_eq!(first_token.to_string(), "1461501637330902918203684832716283019655932542976");
+		assert_eq!(hex_string, "000000000000000000000001d4a24fe19b5e0ed77137012b95b4433293e2ff8e");
+		assert_eq!(first_token.to_string(), "2675427360108358740834871412060616377274126761870");
 	}
 
 	#[test]
@@ -45,13 +46,13 @@ mod tests {
 		// Create the Range
 		let first_slot = Slot::try_from(1).unwrap();
 		let last_slot = Slot::try_from(9).unwrap();
-		let registrant = H160::default();
+		let registrant = H160::from_str("0xD4a24FE19b5e0ED77137012B95b4433293E2Ff8E").unwrap();
 		let range = TokenIdRange::new(first_slot, last_slot, registrant);
 
 		let last_token: U256 = range.last_token().into();
 
 		let hex_string = hex::encode(last_token.to_big_endian());
-		assert_eq!(hex_string, "0000000000000000000000090000000000000000000000000000000000000000");
-		assert_eq!(last_token.to_string(), "13153514735978126263833163494446547176903392886784");
+		assert_eq!(hex_string, "000000000000000000000009d4a24fe19b5e0ed77137012b95b4433293e2ff8e");
+		assert_eq!(last_token.to_string(), "14367440458755582086464350073790880534521587105678");
 	}
 }
