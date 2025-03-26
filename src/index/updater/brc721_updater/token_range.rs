de60e8e726cc990c29a5fda614ea7fab839997ed
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-03-26 12:28:01 +0100

    TokenRange::new

--- src/index/updater/brc721_updater/token_range.rs
+++ src/index/updater/brc721_updater/token_range.rs
@@ -6,3 +6,27 @@ struct TokenRange {
 	slot_rage: RangeInclusive<Slot>,
 	registrant: H160,
 }
+
+impl TokenRange {
+    fn new(first: Slot, last: Slot, registrant: H160) -> Self {
+
+    TokenRange{
+            slot_rage: first..=last,
+            registrant: registrant,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_first_token_id(){
+        // crate the Range
+        let first_slot = Slot::try_from(1).unwrap();
+        let last_slot = Slot::try_from(9).unwrap();
+        let registrant = H160::default();
+        let range = TokenRange::new(first_slot, last_slot, registrant);
+    }
+}
